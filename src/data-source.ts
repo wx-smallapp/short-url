945126d59e2fb1560d5d2962c6ef72852d08c63b
xwx <xu8023shuchang@163.com>
2023-08-18 09:41:41 +0800

    1

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,10 +4,10 @@ import { Shorturl } from "./entity/Shorturl"
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "bj-cynosdbmysql-grp-h3yb8fr0.sql.tencentcdb.com",
-    port: 24967,
-    username: "root",
-    password: "Xu07247857.",
+    host: "",
+    port: 0,
+    username: "",
+    password: "",
     database: "shorturl",
     synchronize: false,
     logging: false,
