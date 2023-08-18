02b60cebf8556caea7fb47faf598d418baf166f1
xwx <xu8023shuchang@163.com>
2023-08-18 10:46:54 +0800

    add loadEnv.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,13 +1,14 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { Shorturl } from "./entity/Shorturl"
+import { MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PWD } from "./loadEnv"
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "",
-    port: 0,
-    username: "",
-    password: "",
+    host: MYSQL_HOST,
+    port: Number.parseInt(MYSQL_PORT),
+    username: MYSQL_USER,
+    password: MYSQL_PWD,
     database: "shorturl",
     synchronize: false,
     logging: false,
---
+++ src/loadEnv.ts
@@ -0,0 +1,4 @@
+export const MYSQL_HOST = process.env.MYSQL_HOST
+export const MYSQL_PORT = process.env.MYSQL_PORT
+export const MYSQL_USER = process.env.MYSQL_USER
+export const MYSQL_PWD = process.env.MYSQL_PWD
